78b46cc78ecc54d37f48510d53f8f44e99442b86
Dmitri Mendis <2dmendis@gmail.com>
2018-07-03 17:11:51 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,7 +38,7 @@ class ApplicationController < Sinatra::Base
     erb :edit
   end 
   
-  post '/posts/:id' do 
+  patch '/posts/:id' do 
     @post = Post.find(params[:id])
     @post.update(:name => params[:name], :content => params[:content])
     redirect '/posts/:id'
